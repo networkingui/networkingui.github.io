2a48f609fbdd7e6b92875c8294b9a2fbd9fa33f8
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-10 08:45:35 -0400

    Create display.js

---
+++ display.js
@@ -0,0 +1,11 @@
+console.log(myNetwork.activate([0,0]));   
+// -> [0.015020775950893527]
+
+console.log(myNetwork.activate([0,1]));  
+// -> [0.9815816381088985]
+
+console.log(myNetwork.activate([1,0]));  
+// ->  [0.9871822457132193]
+
+console.log(myNetwork.activate([1,1]));  
+// -> [0.012950087641929467]
