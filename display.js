448663d3737b5f69ad7bb34ad908f46349d087db
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-13 08:07:47 -0400

    Update display.js

--- display.js
+++ display.js
@@ -1,13 +1,15 @@
-console.log(myNetwork.activate([0,0]));   
+ var a = console.log(myNetwork.activate([0,0]));   
 // -> [0.015020775950893527]
 
-console.log(myNetwork.activate([0,1]));  
+var b = console.log(myNetwork.activate([0,1]));  
 // -> [0.9815816381088985]
 
-console.log(myNetwork.activate([1,0]));  
+var  c = console.log(myNetwork.activate([1,0]));  
 // ->  [0.9871822457132193]
 
-console.log(myNetwork.activate([1,1]));  
+ var d = console.log(myNetwork.activate([1,1]));  
 // -> [0.012950087641929467]
+
+var computerChoice = concat(${a},${b},${c},${d} );
 
 document.getElementById('content').innerHTML = computerChoice
