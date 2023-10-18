6f65bb3d461bfaac72eb9143a5e9e6a55b89e7a7
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-18 10:12:40 -0400

    Update neural.js

--- neural.js
+++ neural.js
@@ -1,15 +1,1 @@
-const { Layer, Network } = window.synaptic;
 
-var inputLayer = new Layer(2);
-var hiddenLayer = new Layer(3);
-var outputLayer = new Layer(1);
-
-
-inputLayer.project(hiddenLayer);
-hiddenLayer.project(outputLayer);
-
-var myNetwork = new Network({
-input: inputLayer,
-hidden: [hiddenLayer],
-output: outputLayer
-});
