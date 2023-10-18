24701897936b842e4c022eabda8800380dc9a112
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-18 13:44:57 -0400

    Update script.js

--- sign/script.js
+++ sign/script.js
@@ -1,10 +1,11 @@
 var ymaluser = user;
 var input = useremail; 
+var mail =  Welcome;
+inputType = "email";
+mailType = "confirmation email";
 
-inputType = "email"
+Welcome="Hello User! Welcome to Neural executive. Thank You for Registering. You will have to read this and verify every log in so dont loose your password! Enter the link below to verify your email in a new tab! Link: https://neuralexecutive.github.io/neuralexecutive/emailhere"
 
 
-OnClick(SendMail(ymaluser="input"));
 
-
-  
+OnCLick(mailto(ymaluser="input"));
