639591e08125ee7a7dd400236798fb3c4971b55e
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-18 10:14:07 -0400

    Create train.js

---
+++ neuralexecutive/train.js
@@ -0,0 +1,27 @@
+// train the network - learn XOR
+var inputtext = document.getElementById('input')
+function getinput() {
+  inputtext.innerHTML = "";
+}
+
+var learningRate = .3;
+
+for (var i = 0; i < 20000; i++) {  
+  // 0,0 => 0 s
+  myNetwork.activate([0,0]);  
+  myNetwork.propagate(learningRate, [0]);
+
+  // 0,1 => 1  
+  myNetwork.activate([0,1]);  
+  myNetwork.propagate(learningRate, [1]);
+
+  // 1,0 => 1  
+  myNetwork.activate([1,0]);  
+  myNetwork.propagate(learningRate, [1]);
+
+  // 1,1 => 0  
+  myNetwork.activate([1,1]);  
+  myNetwork.propagate(learningRate, [0]);  
+}
+
+
