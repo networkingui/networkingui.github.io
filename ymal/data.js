d0718a7e91a7054d67b43030960d7041315bae63
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-20 10:16:02 -0400

    Create data.js

---
+++ ymal/data.js
@@ -0,0 +1,15 @@
+ContentEditable(true);
+
+const User = _user.Data
+  
+var UserInput = NewInput(); 
+
+  var userdata = NewUser();
+    
+Onclick(Submit(UserInput));
+
+ OnCommand(Update(UserData));
+
+ UserInput(Edit(UserData));
+
+// Needs Work 
