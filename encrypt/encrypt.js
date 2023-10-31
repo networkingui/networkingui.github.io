aa8f0cb3b67b4d30b5ad91555e2dcba0522cc5fa
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-10-31 12:57:24 -0400

    Update encrypt.js

--- encrypt/encrypt.js
+++ encrypt/encrypt.js
@@ -1,1 +1,22 @@
 
+var userdata = src="UserData.js";
+
+Get(Userdata);
+
+var patterntype = (
+
+  pattern1 = -.--.--.-..-.-.----..--.-..-.-.-.-..........-----.--.-.--.-...--.-.-.-.-.--.-.
+  pattern2 = -..d;lkd/s,od..djfiejmdjuruh11##%@$^JGugTHJDY&^#*E>>:DODMMXKDIH?>>//////>>MOidgenksuejsnncydj
+  pattern3 = ^^#*33637bd7wehdsudfgwhdud..dlds/x'[.```kdje/spspd'/d''xd.dlcodpelkxjsyuejhfhdfueihjfjudjfhdk
+  pattern4 = qwertyuiopasdfghjklzxcvbnmjudufjy636748587374874747xmnbvcvbmmnbvcxzlkjhgfdaspouioyqwyter
+  pattern5 = 226464tyeygdtrfgye4b gfgydezsxdcfgvbhvgcfdxszdxcfvgybhvgfctrtrxghbj
+  pattern6 = /..;..//'[][;;'/.,.l;llokkhiuhdgwssjfhdyedhfdhgfell,.sli836363723839eyfgdghsqlisoojcjdidkcvyrufehyfef
+);
+
+var patternconcat = concat(Patterntype; if(i++ < PatternType; concat(patterntype)));
+
+get(patternById)
+concat(Patterntype; if(i++ < PatternType; concat(patterntype)));
+Get(patternconcat; mask(userdata = patternconcat; true;));
+If(userdata = patternconcat; true; Display(patternconcat; Vault(userdata);
+
