325d15173fd06fb08a2fa64b777bc5544ef08eff
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2024-01-04 10:29:01 -0500

    Update module1.py

--- python/module1.py
+++ python/module1.py
@@ -3,12 +3,25 @@ Y = bool("True")
 Z = bool("false")
 z = subjects(("math", "ELA", "science", "history", "language"))
 y = "elective"
+c = "complete"
+f = "uncomplete"
 
+print(type(y))
 
+if y > z: 
+    print(Z)
+    print(y = electives(("lang output", "answer output"))
+    print("select new")
+if z = c:
+    print("Tasks completed")
+if z = f:
+    print("Tasks not completed")
+if
+  
 
     
 
 
 
 
-
+ 
