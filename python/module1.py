3bb694fa64d96fc1d336cd49fbd2311d3ecc8ed1
Hack3r5 <115195207+Hideoutcode@users.noreply.github.com>
2024-01-09 12:04:45 -0500

    Create module1.py

---
+++ python/module1.py
@@ -0,0 +1,17 @@
+#states variable user and asks for selection
+os. environ['USER'] = 'New User'
+print("please select a one of the following for an  input.")
+print(y)
+#execute first input, asks for users name input and prints  user input str 
+name = input("please enter your name:")
+    print("hello," + name + "!")
+#user string input, user makes selection and prints choice + your selection for confirmation
+x = input("User selection:")
+    print( "selection = " + x )
+
+
+y = str(("one", "two", "three"))   #choice for selection
+
+
+
+
